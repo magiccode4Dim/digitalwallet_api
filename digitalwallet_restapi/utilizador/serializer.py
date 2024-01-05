09d99025ace68206bbe91fa0334166d284a39e60
Narciso Cadeado <narcisopascoal97@gmail.com>
2024-01-05 19:01:31 +0200

    Relsolvendo problema de Hashs na senha

--- digitalwallet_restapi/utilizador/serializer.py
+++ digitalwallet_restapi/utilizador/serializer.py
@@ -4,5 +4,18 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username','first_name','last_name', 'email', 'password')  
+        fields = ('id', 'username','first_name','last_name', 'email', 'password')
+    def create(self, validated_data):
+        # Extrai a senha dos dados validados
+        password = validated_data.pop('password', None)
+
+        # Cria o usuário sem definir a senha ainda
+        user = User.objects.create(**validated_data)
+
+        # Insere a senha no objecto, transformando-a em hash
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user  
 
