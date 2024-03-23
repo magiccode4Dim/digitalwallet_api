59fea48a39874274fab20863c129fced6bbe6179
Narciso Cadeado <narcisopascoal97@gmail.com>
2024-03-23 13:01:20 +0200

    Primeria Versão - Finalizada

--- digitalwallet_restapi/digitalwallet_restapi/settings.py
+++ digitalwallet_restapi/digitalwallet_restapi/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 # Carrega variáveis de ambiente de .env
-load_dotenv()
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
